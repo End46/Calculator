f7fc610161a857b7099377a716a2366f21cdc169
End46 <165869010+End46@users.noreply.github.com>
2024-04-30 17:50:33 -0400

    Calculator: *Se añadio funciones aritméticas(+-*/) *Se añadio interfaz *Se añadio animacion *Se añadio entrada por teclado(sin animacion) *Se añadio CE y Backspace *Se añadio . para numeros con coma

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Calculator
+commit1:
+*Se añadio funciones aritmeticas
+*Se añadio interfaz
+*Se añadio animacion
+*Se añadio entrada por teclado(sin animacion)
+*Se añadio CE y Backspace
+*Se añadio . para numeros con coma
---
+++ script.js
@@ -0,0 +1,313 @@
+function suma (variableA,variableB){
+    if (variableB==''){
+        variableA = 0;
+        return variableA;
+    }else{
+        variableA=Number(variableA);
+        variableB=Number(variableB);
+        return variableA+variableB;
+    } 
+}
+
+function resta (variableA, variableB){
+    if (variableB==''){
+        variableA = 0;
+        return variableA;
+    }else{
+        return variableA-variableB;
+    } 
+}
+
+function multiplicacion (variableA, variableB){
+    if (variableB==''){
+        variableA = 0;
+        return variableA;
+    }else{
+        return variableA*variableB;
+    }
+}
+
+function división (variableA, variableB){
+    if (variableB==''){
+        variableA = 0;
+        return variableA;
+    }else{
+        if(variableB != 0){
+            return variableA/variableB;
+        }else{
+            alert('La división entre 0 no esta definida');
+            return variableA
+        }
+    }  
+}
+
+
+let variableA = '0';
+let variableB = '';
+let operator = '0';
+let banderaPunto = true;
+let banderaBorrado = true;
+const resultados = document.querySelector('#resultados');
+const botones = document.querySelector('#botones');
+const back = document.querySelector('#backSpace');
+const ce = document.querySelector('#ce');
+
+
+function AccionNumero(valor){
+    if(operator == '0'){
+        if(variableA == 0){
+            variableA = `${valor}`;
+        }else{
+            variableA += `${valor}`;
+        }
+        resultados.textContent=`${variableA}`;
+    }else{
+            variableB += `${valor}`;
+            resultados.textContent=`${variableA}${operator}${variableB}`;
+        }
+}
+
+
+function AccionOperador(valor){
+    if(operator=='0'){
+        operator=valor;
+        banderaPunto=true;
+        resultados.textContent=`${variableA}${operator}`;
+    }else{
+        AccionIgual();
+        operator=valor;
+        banderaPunto=true;
+        resultados.textContent=`${variableA}${operator}`;
+    }
+}
+
+function AccionPunto(){
+    if(operator == '0'){
+        if(banderaPunto){
+            variableA += '.';
+            banderaPunto = false;
+            resultados.textContent=`${variableA}`;
+        }
+    }else{
+        if(banderaPunto){
+            variableB += '.';
+            banderaPunto = false;
+            resultados.textContent=`${variableA}${operator}${variableB}`;
+        }
+    }
+}
+
+function AccionIgual(){
+    switch(operator){
+        case '+':
+            variableA = `${suma(variableA,variableB)}`;
+            operator = '0';
+            variableB = '';
+            resultados.textContent = `${variableA}`;
+            break;
+        case '-':
+            variableA = `${resta(variableA,variableB)}`;
+            operator = '0';
+            variableB = '';
+            resultados.textContent = `${variableA}`;
+            break;
+        case '*':
+            variableA = `${multiplicacion(variableA,variableB)}`;
+            operator = '0';
+            variableB = '';
+            resultados.textContent = `${variableA}`;
+            break;
+        case '/':
+            variableA = `${división(variableA,variableB)}`;
+            operator = '0';
+            variableB = '';
+            resultados.textContent = `${variableA}`;
+            break;
+        case '0':
+            resultados.textContent = `${variableA}`;
+            break;
+    }
+}
+
+function AccionBackErase(){
+    if(operator == '0'){
+        let lenght= variableA.length;
+        variableA = variableA.substring(0,lenght-1);
+        if(variableA == ''){
+            variableA = '0';
+        }
+        resultados.textContent = `${variableA}`;
+    }
+}
+
+function AccionCe(){
+    variableA = '0';
+    variableB = '';
+    operator = '0';
+    resultados.textContent=`${variableA}`;
+}
+
+function TeclaNumero(valor){
+    const boton = document.createElement('button');
+    boton.classList.add('boton');
+    boton.style.flexBasis = `${100/4}%`;
+    boton.textContent = `${valor}`;
+    botones.appendChild(boton);
+    boton.addEventListener('click',()=>{
+        AccionNumero(valor);
+    });
+}
+
+function TeclaOperador(valor){
+    const boton = document.createElement('button');
+    boton.classList.add('boton');
+    boton.style.flexBasis = `${100/4}%`;
+    boton.textContent = `${valor}`;
+    botones.appendChild(boton);
+    boton.addEventListener('click',()=>{
+        AccionOperador(valor);
+    });
+}
+
+function TeclaPunto(){
+    const boton = document.createElement('button');
+    boton.classList.add('boton');
+    boton.style.flexBasis = `${100/4}%`;
+    boton.textContent = '.';
+    botones.appendChild(boton);
+    boton.addEventListener('click',()=>{
+        AccionPunto();
+    });
+}
+
+function TeclaIgual(){
+    const boton = document.createElement('button');
+    boton.classList.add('boton');
+    boton.style.flexBasis = `${100/4}%`;
+    boton.textContent = '=';
+    botones.appendChild(boton);
+    boton.addEventListener('click',()=>{
+        AccionIgual();
+    });
+}
+
+
+for(i=0;i<16;i++){
+    switch(i){
+        case 0:
+            TeclaNumero(7);
+            break;
+        case 1:
+            TeclaNumero(8);
+            break;
+        case 2:
+            TeclaNumero(9);
+            break;
+        case 4:
+            TeclaNumero(4);
+            break;
+        case 5:
+            TeclaNumero(5);
+            break;
+        case 6:
+            TeclaNumero(6);
+            break;
+        case 8:
+            TeclaNumero(1);
+            break;
+        case 9:
+            TeclaNumero(2);
+            break;
+        case 10:
+            TeclaNumero(3);
+            break;
+        case 13:
+            TeclaNumero(0);
+            break;
+        case 12:
+            TeclaPunto();
+            break;
+        case 14:
+            TeclaIgual();
+            break;
+        case 3:
+            TeclaOperador('*');
+            break;
+        case 7:
+            TeclaOperador('/');
+            break;
+        case 11:
+            TeclaOperador('+');
+            break;
+        case 15:
+            TeclaOperador('-');
+            break;
+    }
+    if(banderaBorrado){
+        ce.addEventListener('click',AccionCe);
+        back.addEventListener('click',AccionBackErase);
+        banderaBorrado=false;
+        document.addEventListener('keydown',(event)=>{
+            let key = event.key;
+            console.log(`${event.key}`)
+            switch(key){
+                case '0':
+                    AccionNumero(0);
+                    break;
+                case '1':
+                    AccionNumero(1);
+                    break;
+                case '2':
+                    AccionNumero(2);
+                    break;
+                case '3':
+                    AccionNumero(3);
+                    break;
+                case '4':
+                    AccionNumero(4);
+                    break;
+                case '5':
+                    AccionNumero(5);
+                    break;
+                case '6':
+                    AccionNumero(6);
+                    break;
+                case '7':
+                    AccionNumero(7);
+                    break;
+                case '7':
+                    AccionNumero(7);
+                    break;
+                case '8':
+                    AccionNumero(8);
+                    break;
+                case '.':
+                    AccionPunto();
+                    break;
+                case 'Backspace':
+                    AccionBackErase();
+                    break;
+                case 'Delete':
+                    AccionCe();
+                    break;
+                case '/':
+                    AccionOperador('/');
+                    break;
+                case '*':
+                    AccionOperador('*');
+                    break;
+                case '+':
+                    AccionOperador('+');
+                    break;
+                case '-':
+                    AccionOperador('-');
+                    break;
+                case 'Enter':
+                    AccionIgual();
+                    break;
+            }
+        })
+    }
+    
+}
+
